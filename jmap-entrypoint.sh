2bec6a88c06551ee137b638ee04147bb173d2d16
Noel <gsm731@yandex.ru>
2022-05-11 01:05:21 +0300

    fix liveness probe and save dump with date

--- jmap-entrypoint.sh
+++ jmap-entrypoint.sh
@@ -1,1 +1,1 @@
-jmap -dump:live,format=b,file=/dumps/jmap-dump-$(hostname).bin $(ps aux | grep java | grep -v grep | awk '{print $2}')
+jmap -dump:live,format=b,file=/dumps/jmap-dump-$(hostname)-$(date +%s).bin $(ps aux | grep java | grep -v grep | awk '{print $2}')
